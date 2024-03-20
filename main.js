bed0a81f363455503389b1381fe65e1d5d03ab9e
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2024-03-20 22:06:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status="";
+objects=[];
 function setup()
 {
     canvas=createCanvas(480,380);
@@ -9,7 +10,7 @@ function setup()
 
 function start()
 {
-    objectdetector=ml5.objectDetector('cocossd',modelLoaded);
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
     document.getElementById("status").innerHTML="status : detecting objects";
     value=document.getElementById("inputbox").value;
 }
@@ -23,4 +24,42 @@ function modelLoaded()
 function draw()
 {
     image(video,0,0,480,380);
+
+    if(status!="")
+    {
+        objectDetector.detect(video,gotResult)
+        for(i=0; i<objects.length;i++)
+        {
+            fill("royalblue");
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+percent+"%"+objects[i].x+15,objects[i].y+15);
+            noFill();
+            stroke("cyan");
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            if(value==objects[i].label)
+            {
+                video.stop();
+                objectDetector.detect(gotResult);
+                document.getElementById("status").innerHTML="status :"+value+" is found";
+                synth=window.speechSynthesis;
+                utterthis=new SpeechSynthesisUtterance("object found");
+                synth.speak(utterthis);
+            }
+            else{
+                document.getElementById("status").innerHTML=("status : "+value+" is not found")
+            }
+        }
+    }
+}
+
+function gotResult(error,results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else{
+    console.log(results)
+    objects=results
+    }
 }
